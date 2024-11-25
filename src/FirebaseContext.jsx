a75d7b63712bc9e1afd92e08bcac6a902b6c97b9
Alfikri <83638938+Alfikriangelo@users.noreply.github.com>
2024-11-25 17:07:55 +0700

    perbaikan

--- src/FirebaseContext.jsx
+++ src/FirebaseContext.jsx
@@ -6,17 +6,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBF0gk7aXu5zHS7br7eC-buIrF_TEzdq_Q",
-  authDomain: "sips-ad324.firebaseapp.com",
-  databaseURL:
-    "https://sips-ad324-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "sips-ad324",
-  storageBucket: "sips-ad324.appspot.com",
-  messagingSenderId: "249287359473",
-  appId: "1:249287359473:web:eadb4b2b52b55fee54e250",
-  measurementId: "G-LFZMFD5206",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,12 +1,2 @@
 // firebaseConfig.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyBF0gk7aXu5zHS7br7eC-buIrF_TEzdq_Q",
-  authDomain: "sips-ad324.firebaseapp.com",
-  databaseURL:
-    "https://sips-ad324-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "sips-ad324",
-  storageBucket: "sips-ad324.appspot.com",
-  messagingSenderId: "249287359473",
-  appId: "1:249287359473:web:eadb4b2b52b55fee54e250",
-  measurementId: "G-LFZMFD5206",
-};
+export const firebaseConfig = {};
